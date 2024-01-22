1bd25e0dbc903478c5c27751a6ebfb0f4c8970cb
OzoneMotion <seovelaz@gmail.com>
2024-01-22 16:39:32 -0600

    Modificación al código de calculator y users

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,23 +1,22 @@
 const { default: expect } = require('expect');
-
 const calculator = require('../calculator');
 
-test('adds a + b', () => {
-    //
+test("adds a + b", () => {
+    expect(calculator.sum(1, 2)).toBe(3);
 })
 
-test('substracts a - b', () => {
-    //
+test("substracts a - b", () => {
+    expect(calculator.substract(3, 2)).toBe(1);
 })
 
-test('multiplies a * b', () => {
-    //
+test("multiplies a * b", () => {
+    expect(calculator.multiply(3, 5)).toBe(15)
 })
 
-test('divides a / b', () => {
-    // Consider decimals
+test("divides a / b to", () => {
+    expect(calculator.divide(10, 2)).toBe(5);
 })
 
-test('Raises to power a ^ b', () => {
-    //
+test("Raises to power a ^ b", () => {
+    expect(calculator.power(3, 3)).toBe(27)
 })
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,9 +1,15 @@
-// You already have the methods imported
 const { default: expect } = require('expect');
-const { getUser, getUsers } = require('../users');
+const getUserById = require('../users');
 
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
 
-// READ documentation
+test("get id", () => {
+    expect(getUserById(1)).toEqual({
+        id: 1,
+        username: "hpLover4",
+        email: "sirious90@gmail.com",
+        password: "hArrydotCom"
+    });
+})
